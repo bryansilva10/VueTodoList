85bcb565af05099bba7af2401687ac6d24cf185e
Bryan Guillen <bryanguillen@Bryans-MacBook-Pro.local>
2020-07-18 17:09:12 -0600

    Added Vue Template and Created TodoList component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Hello!</h1>
+    <TodoList></TodoList>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import component
+import TodoList from "./components/TodoList";
 
+//export component
 export default {
-  name: 'App',
+  name: "App",
+  //which components this component has
   components: {
-    HelloWorld
+    TodoList
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  background-color: rgb(66, 66, 66);
+  color: aliceblue;
 }
 </style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,53 @@
+<template>
+  <h1>Hello fro Todo List Template</h1>
+</template>
+
+<script>
+export default {
+  //data or state for the component
+  data() {
+    return {
+      todos: [
+        { todoString: "Buy Car", completed: false },
+        { todoString: "Buy Milk", completed: true },
+        { todoString: "Buy Chocolate", completed: false },
+        { todoString: "Buy Clothes", completed: true }
+      ]
+    };
+  },
+  //methods for component
+  methods: {
+    //method to add an item
+    addTodo(newTodo) {
+      //push new todo to data
+      this.todos.push({
+        todoString: newTodo,
+        completed: false
+      });
+    },
+
+    //method to toggle between completed and uncompleted
+    toggleTodo(todo) {
+      //change current boolean state
+      todo.completed = !todo.completed;
+    },
+
+    //method to edit todo item
+    editTodo(todo, newTodoString) {
+      //update string with new data
+      todo.todoString = newTodoString;
+    },
+
+    //delete item
+    deleteTodo(deleteTodo) {
+      //update array filtering those items that are not marked to be deleted
+      this.todos = this.todos.filter(
+        todo => todo.todoString !== deleteTodo.todoString
+      );
+    }
+  }
+};
+</script>
+
+<style>
+</style>
